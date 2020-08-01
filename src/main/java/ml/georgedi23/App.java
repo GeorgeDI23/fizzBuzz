4fe34a062207dc8e0932c393a72a68fd7f459139
George Ionas <gionas@zipcoders-MacBook-Pro.local>
2020-08-01 18:34:35 -0400

    creating app to run and print class

--- src/main/java/ml/georgedi23/App.java
+++ src/main/java/ml/georgedi23/App.java
@@ -1,5 +1,7 @@
 package ml.georgedi23;
 
+import java.util.Arrays;
+
 /**
  * FizzBuzz implementation
  * Fizz for 3
@@ -9,6 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        FizzBuzz fizzBuzzer = new FizzBuzz();
+        int[] givenArray = new int[100];
+        for(int i = 0; i < 100; i++){
+            givenArray[i] = i+1;
+        }
+
+        String[] fizzBuzzedArray = fizzBuzzer.fizzBuzzArray(givenArray);
+        System.out.println(Arrays.toString(fizzBuzzedArray));
     }
 }
